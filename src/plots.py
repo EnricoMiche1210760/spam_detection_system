05059cce25c5764a2d35a1c37c7abc462e1f0aeb
enrico <enrico.michelon.1996@gmail.com>
2024-07-08 21:33:42 +0200

    wip

--- src/plots.py
+++ src/plots.py
@@ -4,23 +4,25 @@ from sklearn.metrics import confusion_matrix
 
 def print_table_from_classification_report(title, report):
     print('\033[94m' + title+ '\x1b[0m')
-    print('\t\tprecision    recall  f1-score   support')
+    print('\033[1m' + '\t\tprecision    recall  f1-score   support'+ '\033[0m')
     for line in report.split('\n')[2:-5]:
         print(line)
     print('\033[92m'+ "\t-------------------------------------------------"+'\x1b[0m')
     print('\n')
 
-def print_confusion_matrix(y_test, y_pred):
+def print_confusion_matrix(y_test, y_pred, title:str = None):
     '''
     Function to print the confusion matrix
     Parameters:
         y_test: np.array, true labels
         y_pred: np.array, predicted labels
-        columns: list, list of columns
+        title: string, plot title
     '''
     _, ax = plt.subplots(figsize=(7, 5))
     sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap="Blues", ax=ax)
-    ax.set_title(f"Confusion matrix")
+    print(title)
+    if title is not None:
+        ax.set_title(title)
     ax.set_xlabel("Predicted severity")
     ax.set_ylabel("Severity")
     ax.set_xticklabels(labels=['Pred Ham', 'Pred Spam'])
